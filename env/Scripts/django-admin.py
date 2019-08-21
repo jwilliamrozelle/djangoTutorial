95244904d49fe840443d0b11c995089bb459b666
Jeffrey <jwilliamrozelle@gmail.com>
2019-08-21 13:31:25 +0000

    Initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jwill\Documents\Repos\djangoTutorial\djangoTutorial\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
